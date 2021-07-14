8cd17cdc88d386e2f10ddf386b8877a2bae0e570
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-14 10:15:10 +0530

    Updates

---
+++ precache-manifest.fceb51ebf3cb4e87573696c317d26c02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b531a05d3931fe37956a231db38878",
+    "url": "/Lorem-Ipsum-React-Project/index.html"
+  },
+  {
+    "revision": "233dadc9ec9d1eba9a7d",
+    "url": "/Lorem-Ipsum-React-Project/static/css/main.9d994fcb.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/Lorem-Ipsum-React-Project/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lorem-Ipsum-React-Project/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "233dadc9ec9d1eba9a7d",
+    "url": "/Lorem-Ipsum-React-Project/static/js/main.8c679c47.chunk.js"
+  },
+  {
+    "revision": "8a3aecc2abd472a6fc16",
+    "url": "/Lorem-Ipsum-React-Project/static/js/runtime-main.77fb795f.js"
+  }
+]);
